1322a31721bc238df0ac2468485fbd5c8030bcbf
Ákos Molnár <molak06@gmail.com>
2016-01-10 17:32:38 +0100

    update to the weekly project. still has bugs in it: [when jumping trough pictures via buttons, thumbnail does not follows it.]

--- week-7/thursday/weekly_project/script.js
+++ week-7/thursday/weekly_project/script.js
@@ -10,39 +10,54 @@ var images = [
   'http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-72799.jpg',
   'http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-68356.jpg',
   'http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-187019.jpg',
-  'http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-59610.jpg'
+  'http://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-59610.jpg',
+  'img/code1.png',
+  'img/code2.png',
+  'img/code3.png'
 ]
 
 var prev = document.querySelector('.prev');
 var next = document.querySelector('.next');
 var currentPicture = document.querySelector('.currentPicture');
+var pictureIndex = 0;
+
+var thumbnails = document.querySelector('.thumbnails');
+
+function addPicturesToThumbnail(src) {
+  var listedPicture = document.createElement('img');
+  listedPicture.setAttribute('src', src);
+  listedPicture.setAttribute('id', i)
+  thumbnails.appendChild(listedPicture);
+}
+
+for (var i = 1; i < images.length; i++ ) {
+  addPicturesToThumbnail(images[i]);
+}
 
 function changePicture(src) {
   currentPicture.setAttribute('src', src);
+  // console.log(document.getElementsByClassName('id' + pictureIndex));
+
 }
 
-var pictureIndex = 0;
+
 
 function pickNextPicture() {
   if (pictureIndex === images.length - 1) {
     pictureIndex = 0;
-    changePicture(images[pictureIndex])
-    console.log(images[pictureIndex]);
   } else {
-    console.log(images[pictureIndex]);
     pictureIndex++;
-    changePicture(images[pictureIndex]);
   }
+    changePicture(images[pictureIndex]);
 }
 
 function pickPrevPicture() {
   if (pictureIndex === 0) {
     pictureIndex = images.length - 1;
-    changePicture(images[pictureIndex])
   } else {
     pictureIndex--;
-    changePicture(images[pictureIndex]);
   }
+  changePicture(images[pictureIndex]);
 }
 
 prev.addEventListener('click', function() {
@@ -52,3 +67,9 @@ prev.addEventListener('click', function() {
 next.addEventListener('click', function() {
   pickNextPicture();
 });
+
+thumbnails.addEventListener('click', function(event) {
+  document.querySelector('.actual').classList.remove('actual');
+  event.target.classList.add('actual')
+  changePicture(event.target.getAttribute('src'));
+})
